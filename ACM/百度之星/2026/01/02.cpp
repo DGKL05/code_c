fe78f9e6d2e2dbc68cb49032428542482afa3bfb
DGKL <2136315951@qq.com>
2026-08-26 15:48:49 +0800

    update(2026百度之星第一场)：优化02AC代码

--- ACM/百度之星/2026/01/02.cpp
+++ ACM/百度之星/2026/01/02.cpp
@@ -5,8 +5,6 @@ using ll = long long;
 
 struct TopK {
     int K;
-
-    // big 保存最大的 K 个
     multiset<int> small, big;
 
     ll sumBig = 0;
@@ -14,7 +12,7 @@ struct TopK {
     TopK(int k = 0) : K(k) {}
 
     void balance() {
-        // big 太多
+
         while ((int)big.size() > K) {
             auto it = big.begin();
 
@@ -26,7 +24,6 @@ struct TopK {
             small.insert(x);
         }
 
-        // big 太少
         while ((int)big.size() < K && !small.empty()) {
             auto it = prev(small.end());
 
@@ -38,7 +35,6 @@ struct TopK {
             sumBig += x;
         }
 
-        // 保证 big 中全部 >= small 中全部
         while (!small.empty() && !big.empty()) {
             auto it1 = prev(small.end()); // small 最大
             auto it2 = big.begin();       // big 最小
@@ -92,32 +88,20 @@ struct TopK {
     }
 };
 
-void solve() {
+void test() {
     int n, m, k;
     cin >> n >> m >> k;
 
     vector<ll> a(n + 1);
-
-    // value + position
     vector<pair<ll, int>> ord;
 
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
 
-        ord.push_back({
-            a[i],
-            i
-        });
+        ord.push_back({a[i] , i});
     }
 
     sort(ord.begin(), ord.end());
-
-    /*
-        按值分组。
-
-        val[i]     = 第 i 个不同的值
-        pos[i]     = 值为 val[i] 的所有位置
-    */
 
     vector<ll> val;
     vector<vector<int>> pos;
@@ -131,13 +115,6 @@ void solve() {
         pos.back().push_back(p);
     }
 
-    /*
-        zero 表示当前不在 [val[l], val[r]]
-        中的位置。
-
-        即这些位置视为 0。
-    */
-
     set<int> zero;
 
     zero.insert(0);
@@ -148,39 +125,16 @@ void solve() {
 
     zero.insert(n + 1);
 
-    /*
-        维护最大的 m+1 个连续 1 段
-    */
     TopK top(m + 1);
 
-    /*
-        p 从 0 -> 1
-    */
     auto activate = [&](int p) {
 
         auto it = zero.find(p);
 
         int L = *prev(it);
         int R = *next(it);
-
-        /*
-             L ... p ... R
-
-        原来：
-
-        (L,p)
-        (p,R)
-
-        两个 1 段
-
-        现在合并成：
-
-        (L,R)
-        */
-
         int left = p - L - 1;
         int right = R - p - 1;
-
         int merged = R - L - 1;
 
         top.eraseOne(left);
@@ -191,78 +145,32 @@ void solve() {
         zero.erase(it);
     };
 
-    /*
-        p 从 1 -> 0
-    */
     auto deactivate = [&](int p) {
 
         auto it = zero.lower_bound(p);
 
         int R = *it;
         int L = *prev(it);
-
-        /*
-        原来：
-
-        L [111 p 111] R
-
-        一个整体。
-
-        删除 p 后拆成两个。
-        */
-
         int old = R - L - 1;
-
         int left = p - L - 1;
         int right = R - p - 1;
-
         top.eraseOne(old);
-
         top.add(left);
         top.add(right);
-
         zero.insert(p);
     };
 
     ll ans = (1LL << 62);
-
     int l = 0;
-
-    /*
-        双指针枚举值域：
-        [val[l], val[r]]
-    */
-
     for (int r = 0; r < (int)val.size(); r++) {
-
-        // val[r] 加入窗口
         for (int p : pos[r]) {
             activate(p);
         }
-
-        /*
-            如果最大的 m+1 个连续段
-            已经能够提供 >= k 个元素，
-
-            当前值域合法。
-        */
-        while (
-            l <= r &&
-            top.query() >= k
-        ) {
-
-            ans = min(
-                ans,
-                val[r] - val[l]
-            );
-
-            /*
-                缩小左端点
-            */
+        while ( l <= r &&top.query() >= k) {
+            ans = min(ans , val[r] - val[l]);
             for (int p : pos[l]) {
                 deactivate(p);
             }
-
             l++;
         }
     }
@@ -274,12 +182,10 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T;
+    int T = 1;
     cin >> T;
 
-    while (T--) {
-        solve();
-    }
+    while (T--) test();
 
     return 0;
 }
